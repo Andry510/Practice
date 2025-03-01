7c0130d409b8851bb1d090acf4971d8e15faa79a
Andry510 <andrymonteleon@gmail.com>
2025-03-01 02:09:05 -0600

    Creacion y actualizacion de los mensajes
    Se crearon mensajes de error y exito para el usuario.

---
+++ src/messages/error.ts
@@ -0,0 +1,7 @@
+export const ErrorMessage = {
+    login: '¡Ups! Intenta de nuevo.',
+    products: '¡Ups! Algo salió mal con los productos.',
+    updateProduct: '¡Ups! Algo salió mal al actualizar el producto.',
+    notFoundProduct: '¡Ups! El producto no fue encontrado.'
+
+}
---
+++ src/messages/success.ts
@@ -0,0 +1,4 @@
+export const SuccessMessage = {
+    login: `¡Bienvenido, $username!`,
+    updateProduct: '¡Producto actualizado correctamente!'
+}
