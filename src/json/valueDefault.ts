59c8a4d161a704b02ccbe2944b2be40d1f64b7c9
Andry510 <andrymonteleon@gmail.com>
2025-03-01 02:08:24 -0600

    Creación del json
    Se crearon los valores valores predeterminados para el perfil del usuario

---
+++ src/json/valueDefault.ts
@@ -0,0 +1,11 @@
+import { IProfile } from "../interfaces/profile";
+
+export const  profileDefault: IProfile =  {
+    id: 0,
+    username: "",
+    email: "",
+    firstName: "",
+    lastName: "",
+    gender: "male",
+    image: ""
+}
