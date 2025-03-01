3810e77e0046c5f5f81eb7751899960a74de241d
Andry510 <andrymonteleon@gmail.com>
2025-03-01 02:05:20 -0600

    Creación del contexto de la api
    Se creo los contexto de la api con zustand para centralizar la apliación, junto con la logica que se desarrollo

---
+++ src/contexts/api.context.ts
@@ -0,0 +1,108 @@
+//Hooks
+import { toast } from "sonner";
+import { create } from "zustand";
+import { AxiosResponse } from "axios";
+
+//Store 
+import { useStore } from "../store";
+
+//Api
+import { api } from "../api";
+
+//Routers
+import { apiRouters } from "../api/routes";
+
+//Messages
+import { ErrorMessage, SuccessMessage } from "../messages";
+
+//Interfaces
+import { IApiContext } from "../interfaces/contexts";
+import { ILoginResponse, IProductsResponse, IUpdateProductResponse } from "../interfaces/api";
+
+export const useApi = create<IApiContext>(
+    (set, get) => (
+        {
+            products: [],
+
+            //Functions
+            handleLogin: async (data) => {
+                try {
+                    const loginData = {
+                        ...data,
+                        expiresInMins: 1,
+                        credential: 'include',
+                    };
+
+                    const setLogin = useStore.getState().setLogin;
+
+                    const response: AxiosResponse<ILoginResponse> = await api.post(apiRouters.authentication, loginData);
+
+                    const { accessToken, refreshToken, ...profile } = response.data;
+
+                    setLogin({
+                        accessToken,
+                        refreshToken,
+                        profile,
+                    })
+
+
+                    toast.success(SuccessMessage.login.replace('$username', profile.firstName));
+
+                } catch (error) {
+                    toast.error(ErrorMessage.login)
+                }
+            },
+            handleGetProduct: async (id) => {
+                try {
+                    const products = get().products;
+                    return products.find((products) => products.id === id);
+                } catch (error) {
+                    toast.error(ErrorMessage.notFoundProduct)
+                }
+            },
+
+            handleGetProducts: async () => {
+                try {
+                    const response: AxiosResponse<IProductsResponse> = await api.get(apiRouters.products);
+                    set({ products: response.data.products })
+                } catch (error) {
+                    toast.error(ErrorMessage.products)
+                }
+
+            },
+
+            handleCreateProduct: async () => {
+                try {
+
+                } catch (error) {
+                }
+            },
+            handleUpdateProduct: async (data) => {
+                try {
+                    const response: AxiosResponse<IUpdateProductResponse> = await api.put(
+                        apiRouters.update.replace('$productId', data.id.toString()),
+                        {
+                            title: data.title,
+                            description: data.description,
+                        },
+                    )
+
+                    const updatedProducts = get().products.map(
+                        (product) => {
+                            if (product.id === response.data.id)
+                                return { ...product, ...response.data }
+
+                            return product
+                        }
+                    )
+
+                    set({ products: updatedProducts })
+
+                    toast.success(SuccessMessage.updateProduct);
+                } catch (error) {
+                    toast.error(ErrorMessage.updateProduct)
+                }
+            },
+        }
+    )
+)
