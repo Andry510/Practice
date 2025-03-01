4aae2fcfe7602984cf69ee00c7c60273770e65be
Andry510 <andrymonteleon@gmail.com>
2025-03-01 02:09:55 -0600

    Actualización de los esquemas
    Se actualizo los esquemas agregandole las clave secreta de la variable de entonro

--- src/schema/env.schema.ts
+++ src/schema/env.schema.ts
@@ -4,5 +4,5 @@ import { z } from 'zod';
 export const EnvSchema = z.object({
     VITE_BACKEND_URL: z.string().nonempty(),
     VITE_STORE_NAME: z.string().nonempty(),
-    VITE_KEY_CRYPTO: z.string().nonempty(),
+    VITE_KEY_CRYPTO: z.string().nonempty(),    
 }).passthrough();
